7f1f94e14580a91c5a2b1bf01067829ab1dd3825
Thanaphoom Babparn <tp.babparn@gmail.com>
2023-12-14 03:33:12 +0900

    Complete Day-13

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,4 +1,5 @@
-import kotlin.math.max
+import kotlin.math.abs
+
 
 fun main() {
     val input1 = readInput("input-13")
@@ -11,118 +12,82 @@ fun main() {
     output1.println()
 
     // Part2
-//    check(Day12Util.part2(sample1) == 525152L)
-//    val output2 = Day12Util.part2(input1)
-//    check(output2 == 3384337640277L)
-//    output2.println()
+    check(Day13Util.part2(sample1) == 400)
+    val output2 = Day13Util.part2(input1)
+    check(output2 == 23479)
+    output2.println()
 }
 
 private object Day13Util {
 
-    private const val ASH = '.'
-    private const val ROCK = '#'
-
     fun part1(input: List<String>): Int {
         val patterns = parseInput(input)
         return patterns.sumOf {
-            calculateMirrorPattern(it)
+            calculateMirrorPattern(it, 0)
         }
-    }
-
-    private fun calculateMirrorPattern(pattern: List<String>): Int {
-        val maxRow = pattern.size
-        val maxCol = pattern[0].length
-        val allPoints = mutableListOf<Point>()
-        // Store all points
-        for (row in 0 until maxRow) {
-            for (col in 0 until maxCol) {
-                val label = pattern[row][col]
-                val point = Point(label, row, col)
-                allPoints.add(point)
-            }
-        }
-        return countRowMatch(maxRow, allPoints) * 100 + countColMatch(maxCol, allPoints)
-    }
-
-    private fun countRowMatch(maxRow: Int, allPoints: MutableList<Point>): Int {
-        val rangeList = (0 until maxRow).windowed(2)
-        val rowGroup = mutableMapOf<Int, MutableSet<Pair<Char, Int>>>()
-        allPoints.forEach {
-            val row = it.r
-            val col = it.c
-            val set = rowGroup.getOrPut(row) { mutableSetOf() }
-            set.add(it.label to col)
-        }
-        var mirrorCount = 0
-        rangeList.forEach {
-            var (up, down) = it
-            var localCount = 0
-            var isMatch = true
-            while (up >= 0 && down < maxRow) {
-                val upPoints = rowGroup[up]!!
-                val downPoints = rowGroup[down]!!
-                if (upPoints.containsAll(downPoints)) {
-                    localCount++
-                } else {
-                    isMatch = false
-                    break
-                }
-                up--
-                down++
-            }
-            if (isMatch) {
-                val finalizeLocalCount = if (it.first() - localCount <= 0 && down >= maxRow) {
-                    localCount + 1
-                } else {
-                    (localCount + (it.first() - localCount)) + 1
-                }
-                mirrorCount = max(mirrorCount, finalizeLocalCount)
-            }
-        }
-        return mirrorCount
-    }
-
-    private fun countColMatch(maxCol: Int, allPoints: MutableList<Point>): Int {
-        val rangeList = (0 until maxCol).windowed(2)
-        val rowGroup = mutableMapOf<Int, MutableSet<Pair<Char, Int>>>()
-        allPoints.forEach {
-            val row = it.r
-            val col = it.c
-            val set = rowGroup.getOrPut(col) { mutableSetOf() }
-            set.add(it.label to row)
-        }
-        var mirrorCount = 0
-        rangeList.forEach {
-            var (left, right) = it
-            var localCount = 0
-            var isMatch = true
-            while (left >= 0 && right < maxCol) {
-                val leftPoints = rowGroup[left]!!
-                val rightPoints = rowGroup[right]!!
-                if (leftPoints.containsAll(rightPoints)) {
-                    localCount++
-                } else {
-                    isMatch = false
-                    break
-                }
-                left--
-                right++
-            }
-            if (isMatch) {
-                val finalizeLocalCount = if (it.first() - localCount <= 0 && right >= maxCol) {
-                    localCount + 1
-                } else {
-                    (localCount + (it.first() - localCount)) + 1
-                }
-                mirrorCount = max(mirrorCount, finalizeLocalCount)
-            }
-        }
-        return mirrorCount
     }
 
     fun part2(input: List<String>): Int {
         val patterns = parseInput(input)
-        return 0
+        return patterns.sumOf {
+            calculateMirrorPattern(it, 1)
+        }
+    }
+
+    private fun calculateMirrorPattern(pattern: List<String>, smudgeFixAmount: Int): Int {
+        findHorizontalMirror(
+            pattern,
+            smudgeFixAmount
+        )?.let { return it } // Find horizontal mirror first due to more value (I guess)
+
+        findVerticalMirror(
+            pattern,
+            smudgeFixAmount
+        )?.let { return it }
+
+        throw IllegalStateException("No mirror pattern found")
+    }
+
+    private fun findHorizontalMirror(pattern: List<String>, smudgeFixAmount: Int): Int? {
+        val rowIndexRange = 0 until pattern.lastIndex
+        return rowIndexRange.firstNotNullOfOrNull { startIdx ->
+            val mirrorRange = createMirrorRanges(startIdx, pattern.lastIndex)
+            val sumOfDifference = mirrorRange.sumOf { (up, down) ->
+                pattern[up].difference(pattern[down])
+            }
+            // If the mirror range that already zip
+            // When difference become zero - That whole pattern is mirror and able to calculate the distance
+            // Based on part 2 - The smudge fix amount is 1 and only 1 on each pattern
+            // Therefore, instead of 0, Find 1 sum of difference and calculate the distance when found it
+            if (sumOfDifference == smudgeFixAmount) (startIdx + 1) * 100 else null
+        }
+    }
+
+    private fun findVerticalMirror(pattern: List<String>, smudgeFixAmount: Int): Int? {
+        val colIndexRange = 0 until pattern.first().lastIndex
+        return colIndexRange.firstNotNullOfOrNull { start ->
+            val mirrorRange = createMirrorRanges(start, pattern.first().lastIndex)
+            val sumOfDifference = mirrorRange.sumOf { (left, right) ->
+                pattern.columnToString(left).difference(pattern.columnToString(right))
+            }
+            // If the mirror range that already zip
+            // When difference become zero - That whole pattern is mirror and able to calculate the distance
+            // Based on part 2 - The smudge fix amount is 1 and only 1 on each pattern
+            // Therefore, instead of 0, Find 1 sum of difference and calculate the distance when found it
+            if (sumOfDifference == smudgeFixAmount) start + 1 else null
+        }
+    }
+
+    private fun List<String>.columnToString(col: Int): String {
+        return joinToString("") { it[col].toString() }
+    }
+
+    private fun String.difference(other: String): Int {
+        return indices.count { this[it] != other[it] } + abs(length - other.length)
+    }
+
+    private fun createMirrorRanges(start: Int, max: Int): List<Pair<Int, Int>> {
+        return (start downTo 0).zip(start + 1..max)
     }
 
     private fun parseInput(input: List<String>): List<List<String>> =
@@ -131,6 +96,4 @@ private object Day13Util {
             .map {
                 it.split("\n") // Split by line
             }
-
-    data class Point(val label: Char, val r: Int, val c: Int)
 }
