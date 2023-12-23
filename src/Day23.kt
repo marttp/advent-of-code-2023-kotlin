6f6cd9adf76a2112d3d9770f8910fef84d4c95d2
Thanaphoom Babparn <tp.babparn@gmail.com>
2023-12-23 19:04:08 +0900

    Complete Day-23 Part 2 (With searching solution)

--- src/Day23.kt
+++ src/Day23.kt
@@ -14,14 +14,16 @@ fun main() {
     output1.println()
 
     // Part2
+    check(Day23Util.part2(sample1) == 154L)
     val output2 = Day23Util.part2(input1)
-//    check(output2 == 39933)
+    check(output2 == 6226L)
     output2.println()
 }
 
 private object Day23Util {
     private const val PATH = '.'
     private const val FOREST = '#'
+
     // Other are slopes
     private val SLOPE = mapOf(
         '^' to Point2D.NORTH,
@@ -40,10 +42,6 @@ private object Day23Util {
         val end = findEndpoint(grid)
         dfs(grid, start, end, longestPath)
         return longestPath[0]
-    }
-
-    fun part2(input: List<String>): Long {
-        return 0
     }
 
     // Find the longest - Definitely Depth First Search
@@ -100,5 +98,73 @@ private object Day23Util {
 
     private fun parseToGrid(input: List<String>): Array<CharArray> {
         return input.map { it.toCharArray() }.toTypedArray()
+    }
+
+
+    fun part2(input: List<String>): Long {
+        val grid = parseToGrid(input)
+        val start = findStartingPoint(grid)
+        val end = findEndpoint(grid)
+        val adjacency = makeAdjacency(grid)
+        return dfsOptimized(adjacency, start, end, mutableMapOf())!!
+    }
+
+    private fun makeAdjacency(grid: Array<CharArray>): Map<Point2D, Map<Point2D, Long>> {
+        val adjacency: MutableMap<Point2D, MutableMap<Point2D, Long>> = mutableMapOf()
+        // Create adjacency
+        // Default to 1 step
+        for (r in grid.indices) {
+            for (c in grid[0].indices) {
+                if (grid[r][c] != FOREST) {
+                    val neighbors = mutableMapOf<Point2D, Long>()
+                    for (dir in FOUR_DIRECTIONS) {
+                        val nr = r + dir.row
+                        val nc = c + dir.column
+                        if (nr in grid.indices && nc in grid[0].indices && grid[nr][nc] != FOREST) {
+                            neighbors[Point2D(nr, nc)] = 1
+                        }
+                    }
+                    adjacency[Point2D(r, c)] = neighbors
+                }
+            }
+        }
+
+        // Merge neighbors
+        adjacency.keys.toList().forEach { key ->
+            adjacency[key]?.takeIf { it.size == 2 }?.let { neighbors ->
+                val left = neighbors.keys.first()
+                val right = neighbors.keys.last()
+                val totalSteps = neighbors[left]!! + neighbors[right]!!
+                adjacency.getOrPut(left) { mutableMapOf() }.merge(right, totalSteps, ::maxOf)
+                adjacency.getOrPut(right) { mutableMapOf() }.merge(left, totalSteps, ::maxOf)
+                listOf(left, right).forEach { adjacency[it]?.remove(key) }
+                adjacency.remove(key)
+            }
+        }
+        return adjacency
+    }
+
+    private fun dfsOptimized(
+        graph: Map<Point2D, Map<Point2D, Long>>,
+        cur: Point2D,
+        end: Point2D,
+        seen: MutableMap<Point2D, Long>
+    ): Long {
+        if (cur == end) {
+            return seen.values.sum()
+        }
+        var best: Long = 0
+        val neighbors = graph[cur] ?: emptyMap()
+        neighbors
+            .filter { (neighbor, _) -> neighbor !in seen }
+            .forEach { (neighbor, steps) ->
+                seen[neighbor] = steps
+                val res = dfsOptimized(graph, neighbor, end, seen)
+                if (res > best) {
+                    best = res
+                }
+                seen.remove(neighbor)
+            }
+        return best
     }
 }
